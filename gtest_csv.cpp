bf654ae006e215c353529a0f5536ca6072708986
ashur80 <155392852+ashur80@users.noreply.github.com>
2024-02-03 21:09:16 +0100

    Repository initialization
     Initial commit
    
     Changes to be committed:
    	new file:   .gitattributes
    	new file:   .gitignore
    	new file:   CMakeLists.txt
    	new file:   CMakePresets.json
    	new file:   LICENSE
    	new file:   gtest_csv.cpp
    	new file:   gtest_csv.h

---
+++ gtest_csv.cpp
@@ -0,0 +1,17 @@
+﻿// gtest_csv.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "gtest_csv.h"
+
+using namespace std;
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+	// Expect two strings not to be equal.
+	EXPECT_STRNE("hello", "world");
+	// Expect equality.
+	EXPECT_EQ(7 * 6, 42);
+}
+
---
+++ gtest_csv.h
@@ -0,0 +1,8 @@
+﻿// gtest_csv.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
